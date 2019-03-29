888a8ea8bc400981ea983c735f79cdfa092f6575
Chris Nagy <chrisnagy10@yahoo.com>
2019-03-28 20:35:27 -0500

    default to data since yesterday

--- POSTdata.rb
+++ POSTdata.rb
@@ -1,13 +1,15 @@
 require 'net/http'
 
 r = Random.new
+sensorTypes = ["flow_rate", "discharge_elevation", "discharge_pressure", "suction_pressure", "tank_fluid_surface_elevation", "tank_gas_overpressure"]
 
 while true do
-  uri = URI("https://demosensorapi.herokuapp.com/sensors/flow_rate")
-  res = Net::HTTP.post_form(uri, 'timestamp' => Time.now, 'sensorReading' => r.rand(500)/100.0, 'id' => 'mypump')
-
-  puts res
+  for type in sensorTypes do
+    uri = URI("https://demosensorapi.herokuapp.com/sensors/" + type)
+    res = Net::HTTP.post_form(uri, 'timestamp' => Time.now, 'sensorReading' => r.rand(500)/100.0, 'id' => 'mypump')
+    puts res
+  end
   puts ''
 
-  sleep(1)
+  sleep(2)
 end
